ccb71d32fc23a0cae9dbc46f7fef98cd9bccb154
Pratima <gaitrisanu2468@gmail.com>
2022-05-22 21:16:17 +0545

    add store in index page
    Signed-off-by: Pratima <gaitrisanu2468@gmail.com>

--- newcryptocurrency/src/index.js
+++ newcryptocurrency/src/index.js
@@ -2,16 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux'
 import { BrowserRouter as Router} from 'react-router-dom';
 // import reportWebVitals from '../../cryptocurrency/src/reportWebVitals';
 import 'antd/dist/antd.css'
+import store from './app/store'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
   <Router>
+  <Provider store={store}>
   <App />
-  </Router>
-   
+  </Provider>
+   </Router>
   </React.StrictMode>
 );
 
